4d8fcbbf50fdbd872fb03db1cba0ec652790202c
Oltiariq3561 <oltiariq356@gmail.com>
2025-01-22 15:45:47 +0500

    tugadi

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 type MainLayoutProps = {
   children: ReactNode;
 };
